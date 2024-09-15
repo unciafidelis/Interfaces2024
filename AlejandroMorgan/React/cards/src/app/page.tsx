20ee264a40be1aa305fa07e0605deea3bc1e9d92
Alejandro Morgan <MY_NAME@example.com>
2024-09-14 19:22:53 -0600

    Create React Project cards

---
+++ AlejandroMorgan/React/cards/src/app/page.tsx
@@ -0,0 +1,48 @@
+'react-client'
+import "./css.css"
+
+export default function Home() {
+  return (
+    <div className="container">
+      <div className="card">
+        <div className="box">
+          <div className="content">
+            <h2>01</h2>
+            <h3>Card one</h3>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+              eiusmod tempor inciidunt ut laboree et dolore magna aliqua
+            </p>
+            <a href=" #">Read More</a>
+          </div>
+        </div>
+      </div>
+      <div className="card">
+        <div className="box">
+          <div className="content">
+            <h2>02</h2>
+            <h3>Card two</h3>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+              eiusmod tempor inciidunt ut laboree et dolore magna aliqua
+            </p>
+            <a href=" #">Read More</a>
+          </div>
+        </div>
+      </div>
+      <div className="card">
+        <div className="box">
+          <div className="content">
+            <h2>03</h2>
+            <h3>Card three</h3>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+              eiusmod tempor inciidunt ut laboree et dolore magna aliqua
+            </p>
+            <a href=" #">Read More</a>
+          </div>
+        </div>
+      </div>
+    </div> 
+  );
+}
